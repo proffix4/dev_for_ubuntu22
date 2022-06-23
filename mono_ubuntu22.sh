3d53e3f374950f494c4f2e062160ab8c898804a1
Talipov S.N <proffix4@gmail.com>
2022-06-23 15:53:36 +0600

    Add files via upload

---
+++ mono_ubuntu22.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# https://www.mono-project.com/download/stable/#download-lin
+
+sudo apt install gnupg ca-certificates
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
+
+sudo apt install -y mono-devel mono-complete mono-dbg referenceassemblies-pcl ca-certificates-mono mono-xsp4
